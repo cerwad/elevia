1203cd27a7421fbbc2c709790af1f4de92b5e480
cerwad <cedric.barale@gmail.com>
2021-11-18 21:39:16 +0100

    Minor corrections

--- src/main/java/fr/baralecorp/elevia/controller/transferObj/UserDisplay.java
+++ src/main/java/fr/baralecorp/elevia/controller/transferObj/UserDisplay.java
@@ -126,7 +126,6 @@ public class UserDisplay {
                 ", birthDate=" + birthDate +
                 ", handle='" + handle + '\'' +
                 ", family='" + family + '\'' +
-                ", password='" + password + '\'' +
                 '}';
     }
 }
